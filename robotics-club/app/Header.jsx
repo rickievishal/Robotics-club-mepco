da3901438438b536272828fc2dcbea26cb7cfe31
rickievishal <rickievishalyt@gmail.com>
2026-01-06 19:04:17 +0530

    fixed the href for the logo to redirect to the home in the nav bar

--- robotics-club/app/Header.jsx
+++ robotics-club/app/Header.jsx
@@ -172,7 +172,7 @@ const Header = () => {
   return (
     <nav className='fixed top-0 w-full h-[50px] bg-black/50 backdrop-blur-3xl text-[var(--primary)] z-40'>
        <ol className='w-full h-full max-w-7xl mx-auto flex justify-between items-center px-2 sm:px-4'>
-       <Link href={user ? "/club/welcome" : "/"}>
+       <Link href={"/"}>
         <li className='text-xl font-bold tracking-[-1px] cursor-pointer'>
           RoboticsClub.
         </li>
